1eb67d7021c55a85645fe5da2631bba985d61ab7
Honesty <honestymoore77@gmail.com>
2023-02-16 10:49:02 -0600

    created routes and components

---
+++ src/pages/ShowPage/ShowPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from '../../components/Post/Post'
+import CommentList from '../../components/CommentList/CommentList'
+
+function ShowPage() {
+  return (
+    <>
+    <Post/>
+    <CommentList/>
+    </>
+  )
+}
+
+export default ShowPage
