8a4556714972bbaa3ee839841c718bc018ac168b
Michael DiFelice <michaeldifelice@michaels-mbp.myfiosgateway.com>
2023-02-19 09:45:32 -0500

    added likes and dislikes to comments.
    > Co-authored-by: Greg <gharmon327@gmail.com>
    > Co-authored-by: Honesty <honestymoore77@gmail.com>
    Co-authored-by:Brian  <btriplett1990@yahoo.com>

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,20 @@
 import * as postsAPI from '../../utilities/posts-api'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Post({post, user}) {
+  const [likeTotal, setLikeTotal] = useState(post.likes.length - post.dislikes.length)
   const navigate = useNavigate()
+
+    async function likePost() {
+        const updatedPost = await postsAPI.likePost(post._id)
+        setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+    }
+
+    async function dislikePost() {
+        const updatedPost = await postsAPI.dislikePost(post._id)
+        setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+    }
 
   async function handleDelete(postId) {
     await postsAPI.deletePost(postId)
@@ -19,6 +31,9 @@ export default function Post({post, user}) {
       <p>{post.text}</p>
       {post.owner === user._id ? <button onClick={() => handleDelete(post._id)}>Delete</button> : null}
       {post.owner === user._id ? <button onClick={() => handleClick(post._id)}>Update</button> : null}
+      <button onClick={likePost}>Like</button>
+                {likeTotal}
+            <button onClick={dislikePost}>Dislike</button>
     </div>
   )
 }
--- src/utilities/comment-api.js
+++ src/utilities/comment-api.js
@@ -10,3 +10,17 @@ export async function deleteComment(postId, commentId) {
     await sendRequest(`${BASE_URL}/${postId}`, 'DELETE', commentId)
     return 
 }
+
+//Adding Like and Dislike to Comments 
+
+export async function likeComment(postId,commentId) {
+    const likedComment = await sendRequest(`${BASE_URL}/likes/${postId}`,'PATCH', commentId)
+    return likedComment
+}
+
+//Body needs to include the comment.id
+
+export async function dislikeComment(postId,commentId) {
+   const dislikedComment = await sendRequest(`${BASE_URL}/dislikes/${postId}`,'PATCH', commentId)
+    return dislikedComment
+}
