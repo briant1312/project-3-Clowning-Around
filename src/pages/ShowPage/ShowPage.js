62a51240944149e602cb91c0bed371fe1b9f3f25
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-02-19 10:26:54 -0500

    fix bug with displaying post likes.
    Co-authored-by: Michael <michael.d.difelice@gmail.com>
    Co-authored-by: Greg <gharmon327@gmail.com>
    Co-authored-by: Honesty <honestymoore77@gmail.com>

--- src/pages/ShowPage/ShowPage.js
+++ src/pages/ShowPage/ShowPage.js
@@ -8,6 +8,8 @@ import CommentInput from '../../components/CommentInput/CommentInput'
 function ShowPage({user}) {
   const [post, setPost] = useState({})
   const [comments, setComments] = useState([])
+  const [likeTotal, setLikeTotal] = useState(0 && post.likes.length - post.dislikes.length)
+
 
   const { postId } = useParams()
 
@@ -16,13 +18,30 @@ function ShowPage({user}) {
       const post = await postsAPI.show(postId)
       setPost(post)
       setComments(post.comments)
+      setLikeTotal(post.likes.length - post.dislikes.length)
     }
     getPost(postId)
   }, [])
 
+  async function likePost() {
+    const updatedPost = await postsAPI.likePost(post._id)
+    setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+}
+
+async function dislikePost() {
+    const updatedPost = await postsAPI.dislikePost(post._id)
+    setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+}
+
   return (
     <>
-    <Post user={user} post={post}/>
+    <Post 
+      likeTotal={likeTotal}
+      user={user} 
+      post={post}
+      likePost={likePost}
+      dislikePost={dislikePost}
+    />
     <CommentInput setComments={setComments} user={user} />
     <CommentList setPost={setPost} setComments={setComments} user={user} comments={comments}/>
     </>
