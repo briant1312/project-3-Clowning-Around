f69a213d032571dd2d9e8763b30a64ac12346ee2
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-02-21 13:38:19 -0500

    build project

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
+const cors = require('cors')
 
 // always require and configure near the top
 require('dotenv').config()
@@ -14,22 +15,23 @@ const PORT = process.env.PORT || 3001
 
 app.use(logger('dev'))
 app.use(express.json())
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 app.use(require('./config/checkToken'))
 
 // configure both the server-favicon and the static middleware
 // to server from the production build folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 // telling our express app to use this directory for our static assets
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/post', require('./routes/api/post'))
 app.use('/api/comment', require('./routes/api/comment'))
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
